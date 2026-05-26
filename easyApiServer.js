e64832e8e6eecdeaf35d33dacbda03fe411f015a
mark-hsieh <mark-hsieh@bies-design.com>
2026-05-26 18:06:31 +0800

    chore. 增加錯誤回報的清晰度

--- easyApiServer.js
+++ easyApiServer.js
@@ -83,9 +83,10 @@ const server = http.createServer((req, res) => {
                                 res.statusCode = 200;
                                 res.end(JSON.stringify({ 
                                     status: "completed", 
-                                    message: "GLB 轉換成功，但材質包壓縮失敗", 
+                                    message: `GLB 轉換成功，但材質包壓縮失敗`, 
+                                    error: error.message,
                                     stdout: stdout,
-                                    zip_error: zipError.message
+                                    stderr: stderr
                                 }));
                             } else {
                                 console.log(`[✅ 壓縮材質包成功] 儲存至: ${path.join(output_dir, fileNameWithoutExt + '.bzip2')}`);
@@ -105,7 +106,8 @@ const server = http.createServer((req, res) => {
                         res.end(JSON.stringify({ 
                             status: "completed", 
                             message: "GLB 轉換成功，無材質包不必壓縮", 
-                            stdout: stdout 
+                            stdout: stdout,
+                            error: "No materials folder generated, skipping compression step." // 這裡的 error 欄位只是為了讓 A 電腦能夠在收到回應後知道為什麼沒有壓縮包，而不是實際的錯誤 
                         }));
                     }
                 });
